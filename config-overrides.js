c47bc51bdbb1b54c0d22ae1168487116552a0834
DKFN <valere.tetelin@gmail.com>
2021-10-05 16:26:11 +0200

    Some cleanup

--- config-overrides.js
+++ config-overrides.js
@@ -11,12 +11,12 @@ module.exports = {
 
             // Overrides from https://github.com/facebook/create-react-app/issues/3855#issuecomment-406653733
             //JS Overrides
-            config.output.filename = 'static/js/[name].js';
-            config.output.chunkFilename = 'static/js/[name].chunk.js';
+            config.output.filename = 'static/[name].js';
+            config.output.chunkFilename = 'static/[name].chunk.js';
 
             //CSS Overrides
-            config.plugins[5].options.filename = 'static/css/[name].css';
-            config.plugins[5].options.chunkFilename = 'static/css/[name].css';
+            config.plugins[5].options.filename = 'static/[name].css';
+            config.plugins[5].options.chunkFilename = 'static/[name].css';
         }
         return config;
     }
